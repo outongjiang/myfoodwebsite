4de14fb6549d9bad174b5edf26e62468837b8a68
1111 <3225700692@qq.com>
2021-12-11 23:57:32 +0800

    login页面99%完成

--- src/Test/test1.java
+++ src/Test/test1.java
@@ -1,16 +1,62 @@
 package Test;
-
+import com.sun.corba.se.spi.ior.ObjectKey;
 import util.JDBC;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-class student{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+class student{
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    String phone;
+    public void getAge(){
+        System.out.println(30);
+    }
+    public void getName(){
+        System.out.println("张三");
+    }
 }
 public class test1 {
-    public static void main(String[] args) throws SQLException {
+    public static void invoke(String methodName) throws Exception {
+        Class c=student.class;
 
-
+        Method m =c.getMethod("get"+methodName,null);
+        Object stu=c.newInstance();
+        m.invoke(stu,null);
+    }
+    public static void setFields(student stu, Map<String,String[]>params) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class c=stu.getClass();
+        for(String key:params.keySet()){
+            Method m=c.getMethod(key);
+            for(String value:params.get(key))
+            m.invoke(stu,value);
+        }
+    }
+    public static void main(String[] args) throws SQLException, Exception {
+//        invoke("Name");
+//        student stu=new student();
+//        List<Object>params=new ArrayList<>();
+//        params.add("张三");
+//        params.add(30);
+//        params.add("18023040732");
+//        setFields(stu,params);
+//        System.out.println(stu);
 
     }
 }
---
+++ web/Test/test3.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 86180
+  Date: 2021/12/11
+  Time: 23:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form method="get" action="ServletTest1">
+        name<input name="username" type="text"><br>
+        age<input name="age" type="text"><br>
+        phone<input name="phone" type="text"><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
