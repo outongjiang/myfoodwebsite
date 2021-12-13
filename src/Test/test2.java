6e53276bf517cbb3c569921822fcd2f8af6c392a
1111 <3225700692@qq.com>
2021-12-13 14:54:17 +0800

    el表达式

--- src/Test/test2.java
+++ src/Test/test2.java
@@ -1,4 +1,17 @@
 package Test;
-
-public class test2 {
+class class1{
+    public static void print2(){
+        System.out.println("class1的print2");
+    }
+}
+ class class2 {
+    public  static  void print(){
+        System.out.println("class2的print");
+        class1.print2();
+    }
+}
+public class test2{
+    public static void main(String[] args) {
+        class2.print();
+    }
 }
