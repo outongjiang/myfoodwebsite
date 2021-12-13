6788f768c8b5bbb3b9bf3c2a226c3f2d4eaa726e
1111 <3225700692@qq.com>
2021-12-14 00:24:34 +0800

    el表达式

--- src/dataAccessObject/FoodDao.java
+++ src/dataAccessObject/FoodDao.java
@@ -4,6 +4,7 @@ import model.Food;
 import util.JDBC;
 import util.myJavaBean;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -17,9 +18,19 @@ public class FoodDao {
     }
 
     public List<Food> findFood() throws SQLException {
-        String sql="select * from food";
-        ResultSet rs=JDBC.select(sql,null);
-//        return myJavaBean.Result_List(rs,Food.class,new Food());
+        String sql = "select * from food";
+        ResultSet rs = JDBC.select(sql, null);
+        try {
+            return myJavaBean.Result_List(rs, Food.class);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
