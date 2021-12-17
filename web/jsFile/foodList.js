be7319061c7f5fe8851c15e397250934ba317740
欧桐江 <3210963178@qq.com>
2021-12-17 15:00:02 +0800

    1

--- web/jsFile/foodList.js
+++ web/jsFile/foodList.js
@@ -37,8 +37,8 @@ function edit(id1) {
     var id = "#" + id1;
     if($(id).html() == "编辑"){
         var input =$(id).parent().parent().children("td").children("input[type='text']").not("#bkbj");
-        input.attr("readOnly", false);
-        input.attr("disabled", false);
+        input.prop("readOnly", false);
+        input.prop("disabled", false);
         input.removeClass("foodProperties_border");
         input.focus();
         $(id).html("完成");
